e8bbbce175940ae69db64a89a95a4ba349a0d0ef
wangxiyang <ssdutwxy@foxmail.com>
2017-06-15 00:01:06 +0800

    Add a sockets demo based on Flask

---
+++ paraServer/server_demo.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from flask import Flask
+from flask_sockets import Sockets
+
+
+app = Flask(__name__)
+sockets = Sockets(app)
+
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while not ws.closed:
+        message = ws.receive()
+        ws.send(message)
+
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+if __name__ == "__main__":
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 5000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
